f8f6534909a4edde578cf35d8c56b13dc99d210c
neki375 <34090643+neki375@users.noreply.github.com>
2018-04-19 15:34:10 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -4,12 +4,12 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $textarea = $_POST['textarea'];
 
-    $to = "mail@gmail.com";
+    $to = "SovremennieTehnologii@gmail.com;
     $subject = $name;
     $message = $textarea;
     $headers  = "MIME-Version: 1.0\r\n";
     $headers .= "Content-type: text/html; Charset=utf-8\r\n";
-    $headers .= "From: ООО ЛИДЕРТЕХСТРОЙ <$email>\r\n";
+    $headers .= "From: ООО CОВРЕМЕННЫЕ ТЕХНОЛОГИИ <$email>\r\n";
     mail($to, $subject, $message, $headers);
     echo("Спасибо! Ваше сообщение отправлено");
 ?>
